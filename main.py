541bfcb293852fc12a7afe229d97d34d558b1db1
Sai-Chandar <n.saichander@gmail.com>
2021-11-23 19:52:16 -0500

    plot to show the regression line

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from deap import base, creator, gp, tools, algorithms
 import operator, math, random, numpy
 import pandas as pd
+import matplotlib.pyplot as plt
+import networkx as nx
 
 df = pd.read_csv("data1.csv")
 
@@ -21,8 +23,8 @@ pset.addPrimitive(operator.sub, 2)
 pset.addPrimitive(operator.mul, 2)
 pset.addPrimitive(protectedDiv, 2)
 pset.addPrimitive(operator.neg, 1)
-pset.addPrimitive(math.cos, 1)
-pset.addPrimitive(math.sin, 1)
+# pset.addPrimitive(math.cos, 1)
+# pset.addPrimitive(math.sin, 1)
 pset.addEphemeralConstant("c", lambda: random.uniform(-10000, 10000))
 
 pset.renameArguments(ARG0 = 'x')
@@ -47,8 +49,8 @@ toolbox.register("mate", gp.cxOnePoint)
 toolbox.register("expr_mut", gp.genFull, min_=0, max_=2)
 toolbox.register("mutate", gp.mutUniform, expr=toolbox.expr_mut, pset = pset)
 
-toolbox.decorate("mate", gp.staticLimit(key=operator.attrgetter("height"), max_value = 17))
-toolbox.decorate("mutate", gp.staticLimit(key=operator.attrgetter("height"), max_value = 17))
+toolbox.decorate("mate", gp.staticLimit(key=operator.attrgetter("height"), max_value = 3))
+toolbox.decorate("mutate", gp.staticLimit(key=operator.attrgetter("height"), max_value = 3))
 
 stats_fit = tools.Statistics(lambda ind: ind.fitness.values)
 stats_size = tools.Statistics(len)
@@ -58,9 +60,19 @@ mstats.register("std", numpy.std)
 mstats.register("min", numpy.min)
 mstats.register("max", numpy.max)
 
-pop = toolbox.population(n= 300)
+pop = toolbox.population(n= 400)
 hof = tools.HallOfFame(1)
 
-pop, log = algorithms.eaSimple(pop, toolbox, 0.5, 0.1, 250, stats = mstats, halloffame = hof, verbose=True)
+pop, log = algorithms.eaSimple(pop, toolbox, 0.6, 0.1, 100, stats = mstats, halloffame = hof, verbose=True)
 
 print("Best_fintess_value:",  hof[0].fitness.values, "\nRegression_equation_for_the_best:", str(gp.PrimitiveTree(hof[0])))
+
+func = toolbox.compile(expr = hof[0])
+
+x = list(range(1, 255))
+y = [func(i) for i in x]
+
+
+plt.plot(x, y)
+plt.scatter(X, Y)
+plt.show()
