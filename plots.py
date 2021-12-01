2e393eff192327bf0bf84469b5686524812c2839
Sai-Chandar <n.saichander@gmail.com>
2021-11-30 19:13:12 -0500

    adding cov_plots

--- plots.py
+++ plots.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("./solutions/problem1/cov_data/cov_data_pset3_e_0.6_0.5_300.csv")
+# df = pd.read_csv("./solutions/problem1/cov_data/cov_data_pset3_e_0.6_0.5_300.csv")
+
+df = pd.read_csv("./solutions/problem2/cov_data/cov_data_pset2_0.4_0.3_400.csv")
 
 # df = df[["fit_run_5", "fit_run_10", "fit_run_15", "fit_run_20", "fit_run_25"]]
 
@@ -12,11 +14,11 @@ data = df.iloc[list(range(5, 100, 5))]
 print(data)
 
 plt.boxplot(data.T)
-plt.title("Average convergence rate for pset3_e_0.6_0.5_300")
+plt.title("Average convergence rate for pset2_0.4_0.3_400")
 plt.xlabel("Generations")
 plt.ylabel("Fitness value")
 plt.xticks(list(range(1, 20)), list(range(5, 100, 5)))
 
-plt.savefig("./solutions/problem1/plots/cov_plot/pset3_e_0.6_0.5_300.png")
+plt.savefig("./solutions/problem2/plots/cov_plot/pset2_0.4_0.3_400.png")
 
 
